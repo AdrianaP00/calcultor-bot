8f605d45991dd8d5500f37c843918b06218b1b8c
El_Giancar <giancarmine.cuofano@gmail.com>
2023-05-28 11:49:59 +0200

    add autodeploy automation

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import { Bot, webhookCallback } from "grammy/web";
 // The following line of code assumes that you have configured the secrets BOT_TOKEN and BOT_INFO.
 // See https://developers.cloudflare.com/workers/platform/environment-variables/#secrets-on-deployed-workers.
 // The BOT_INFO is obtained from `bot.api.getMe()`.
-const bot = new Bot("6107196857:AAHADvNYpZFqLc6GgvgSPN1LMq5SO5A0QLY", { botInfo: BOT_INFO });
+const bot = new Bot("6107196857:AAHADvNYpZFqLc6GgvgSPN1LMq5SO5A0QLY");
 
 bot.command("start", async (ctx) => {
   await ctx.reply("Hello, world!");
