3e6183f7ab466402bd87dbe6ae03496876040eb4
El_Giancar <giancarmine.cuofano@gmail.com>
2023-06-04 19:19:41 +0200

    fixed use secrets

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import { Bot, webhookCallback } from "grammy/web";
 // The following line of code assumes that you have configured the secrets BOT_TOKEN and BOT_INFO.
 // See https://developers.cloudflare.com/workers/platform/environment-variables/#secrets-on-deployed-workers.
 // The BOT_INFO is obtained from `bot.api.getMe()`.
-const bot = new Bot(process.env.BOT_TOKEN);
+const bot = new Bot(BOT_TOKEN);
 
 bot.command("start", async (ctx) => {
   await ctx.reply("Hello W!");
